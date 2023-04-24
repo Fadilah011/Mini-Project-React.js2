c2dba0f99c22ba93515a0232f9897188a5e60dbf
unknown <fadilah0887@gmail.com>
2023-04-24 08:09:56 -1100

    initial commit

--- client/src/api/modules/user.api.js
+++ client/src/api/modules/user.api.js
@@ -1,30 +1,81 @@
-import axios from "axios";
-import queryString from "query-string";
+import privateClient from "../client/private.client";
+import publicClient from "../client/public.client";
 
-const baseURL = "https://sunstream1.vercel.app/";
+const userEndpoints = {
+  signin: "user/signin",
+  signup: "user/signup",
+  getInfo: "user/info",
+  passwordUpdate: "user/update-password",
+  signinGoogle: "user/signinGoogle",
+  signupGoogle: "user/signupGoogle",
+};
 
-const privateClient = axios.create({
-  baseURL,
-  paramsSerializer: {
-    encode: params => queryString.stringify(params)
-  }
-});
+const userApi = {
+  signin: async ({ username, password }) => {
+    try {
+      console.log("send request");
+      const response = await publicClient.post(
+        userEndpoints.signin,
+        { username, password }
+      );
 
-privateClient.interceptors.request.use(async config => {
-  return {
-    ...config,
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${localStorage.getItem("actkn")}`
+      return { response };
+    } catch (err) { console.log("err"); return { err }; }
+  },
+  signup: async ({ username, password, confirmPassword, displayName }) => {
+    try {
+      const response = await publicClient.post(
+        userEndpoints.signup,
+        { username, password, confirmPassword, displayName }
+      );
+
+      return { response };
+    } catch (err) { return { err }; }
+  },
+  getInfo: async () => {
+    try {
+      const response = await privateClient.get(userEndpoints.getInfo);
+
+      return { response };
+    } catch (err) { return { err }; }
+  },
+  passwordUpdate: async ({ password, newPassword, confirmNewPassword }) => {
+    try {
+      const response = await privateClient.put(
+        userEndpoints.passwordUpdate,
+        { password, newPassword, confirmNewPassword }
+      );
+
+      return { response };
+    } catch (err) { return { err }; }
+  },
+  signinGoogle: async ({ name, email }) => {
+    try {
+      console.log("send request", name);
+      console.log("send , email", email);
+      const response = await publicClient.post(userEndpoints.signinGoogle, {
+        name,
+        email,
+      });
+      console.log("responsegooleƒ", response);
+      return { response };
+    } catch (err) {
+      console.log("err");
+      return { err };
     }
-  };
-});
+  },
+  signupGoogle: async ({ name, email }) => {
+    try {
+      const response = await publicClient.post(userEndpoints.signupGoogle, {
+        name,
+        email,
+      });
 
-privateClient.interceptors.response.use((response) => {
-  if (response && response.data) return response.data;
-  return response;
-}, (err) => {
-  throw err.response.data;
-});
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+};
 
-export default privateClient;
+export default userApi;
